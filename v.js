dd38c819cf0d7ecb9a1d4991c68c9550179d97ac
ammieeli <1738933182@qq.com>
2017-10-13 19:02:10 +0800

    w

--- v.js
+++ v.js
@@ -1,9 +1,18 @@
 function Drag(id) {
+<<<<<<< HEAD
 	box.addEventListener('mousedown',down);
     function down(ev){
         var disX = ev.clientX - this.offsetLeft;
         var disY = ev.clientY - this.offsetTop;
+        function move(ev){
+            box.style.left = ev.clientX - disX + 'px'; 
+            box.style.top = ev.clientY - disY + 'px';
+        }
+        
         ev.preventDefault();
         
     }
+
+	
+
 }
