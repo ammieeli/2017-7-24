4378b42b789be285404998cb7b7818af73c06389
ammieeli <1738933182@qq.com>
2017-10-13 18:55:45 +0800

    down

--- v.js
+++ v.js
@@ -1,3 +1,9 @@
 function Drag(id) {
-	
+	box.addEventListener('mousedown',down);
+    function down(ev){
+        var disX = ev.clientX - this.offsetLeft;
+        var disY = ev.clientY - this.offsetTop;
+        ev.preventDefault();
+        
+    }
 }
